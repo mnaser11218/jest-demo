74fd1b38e0c2b8a7d5c4e55e7411b19f7e340c8c
Mohammed Naser <mnaser11218@gmail.com>
2025-08-28 11:31:51 -0400

    refactored counter test component to include describe method

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -1,28 +1,38 @@
 import { Counter } from "./Counter";
 import { render, screen } from "@testing-library/react";
-it("renders correctly", () => {
-    render(<Counter description="Counter 1" defaultCount={0} />);
-    expect(screen.getByText("Current Count: 0")).toBeInTheDocument();
-    expect(screen.getByText(/my counter/i)).toBeInTheDocument();
-});
-it("test increment button", () => {
-    render(<Counter description="Counter 1" defaultCount={0} />);
-    expect(screen.getByText("DESC: Counter 1 - DC: 0")).toBeInTheDocument();
-    expect(screen.getByText("Current Count: 0")).toBeInTheDocument();
 
-    const incrementButton = screen.getByLabelText("Add to Counter");
-    incrementButton.click();
+describe("Counter Component", () => {
+    beforeEach(() => {
+        render(<Counter description="Counter 1" defaultCount={0} />);
+    });
 
-    expect(screen.getByText("Current Count: 1")).toBeInTheDocument();
-});
 
-it("test decrement button", () => {
-    render(<Counter description="Counter 1" defaultCount={0} />);
-    expect(screen.getByText("DESC: Counter 1 - DC: 0")).toBeInTheDocument();
-    expect(screen.getByText("Current Count: 0")).toBeInTheDocument();
+    it("renders description and default count", () => {
+        expect(screen.getByText(/counter 1/i)).toBeInTheDocument();
+        expect(screen.getByText("Current Count: 0")).toBeInTheDocument();
+    });
 
-    const decrementButton = screen.getByLabelText("Subtract from Counter");
-    decrementButton.click();
+    describe("Counter Increment Button", () => {
+        beforeEach(() => {
+            const incrementButton = screen.getByLabelText("Add to Counter");
+            incrementButton.click();
+        })
+        it("test increment button", () => {
+            expect(screen.getByText("Current Count: 1")).toBeInTheDocument();
+        });
+    })
 
-    expect(screen.getByText("Current Count: -1")).toBeInTheDocument();
-});
+    describe("Counter Decrement Button", () => {
+        beforeEach(() => {
+            const decrementButton = screen.getByLabelText("Subtract from Counter");
+            decrementButton.click();
+        })
+        it("test decrement button", () => {
+            expect(screen.getByText("Current Count: -1")).toBeInTheDocument();
+        });
+
+    })
+})
+
+
+
